1f735a6282bbdd05629978444cf9daabde447990
Sosuke Suzuki <sosuke.suzuki@dr-ubie.com>
2023-02-26 22:07:28 +0900

    Initial commit

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/sosukesuzuki/simple-kvs
+
+go 1.19
---
+++ simplekvs.go
@@ -0,0 +1,11 @@
+package simplekvs
+
+type SimpleKVS struct {}
+
+func (kvs *SimpleKVS) Set() {}
+
+func (kvs *SimpleKVS) Get() {}
+
+func (kvs *SimpleKVS) Update() {}
+
+func (kvs *SimpleKVS) Delete() {}
